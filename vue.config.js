56fac2e2df1a978654be2b6e8a813399ce285ce2
JiYu <146607445+cherrybbang@users.noreply.github.com>
2024-08-27 10:00:40 +0900

    코드 추가

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer : {
+    webSocketServer: false, //사용하지 않게 변경
+  }
 })
