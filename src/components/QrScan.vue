f79e5eb204b62907111287663eef63a668055eb4
JiYu <146607445+cherrybbang@users.noreply.github.com>
2024-08-28 16:20:20 +0900

    update

--- src/components/QrScan.vue
+++ src/components/QrScan.vue
@@ -36,38 +36,68 @@ export default {
 <template>
     <p>결제하기 - QR 스캔</p>
     <!-- <button @click="decodeQRCode()">Decode QR Code</button> -->
+    <video ref="video" width="90%" height="200" style="border: 1px solid red" autoplay></video>
     <p v-if="result">Result: {{ result }}</p>
     <p v-if="error">Error: {{ error }}</p>
 </template>
 
 <script>
-import { BrowserQRCodeReader } from '@zxing/browser';
+// import { BrowserQRCodeReader } from '@zxing/browser';
+import { BrowserMultiFormatReader } from '@zxing/browser';
 
 export default {
     data() {
         return {
-            imageUrl: '',
-            result: '',
-            error: ''
+            // imageUrl: '',
+            // result: '',
+            // error: ''
+            result : null,
+            codeReader : null,
+            error : ''
         }
     },
-    mounted() {
-        this.decodeQRCode();
+    async mounted() {
+        // this.decodeQRCode();
+        this.codeReader = new BrowserMultiFormatReader();
+        this.StartScanner();
     },
     methods : {
-        async decodeQRCode() {
+        // async decodeQRCode() {
             
-            this.result = '';
-            this.error = '';
+        //     this.result = '';
+        //     this.error = '';
 
+        //     try {
+        //         const codeReader = new BrowserQRCodeReader();
+        //         const result = await codeReader.decodeFromImageUrl(this.imageUrl);
+        //         this.result = result.text;
+        //     } catch (err) {
+        //         console.log(err);
+        //         this.error = err.message;
+        //     }
+        // }
+        async StartScanner() {
             try {
-                const codeReader = new BrowserQRCodeReader();
-                const result = await codeReader.decodeFromImageUrl(this.imageUrl);
-                this.result = result.text;
+                const videoElement = this.$refs.video;
+                const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
+                videoElement.srcObject = stream;
+
+                this.codeReader.decodeFromVideoDevice(null, videoElement, (result, err) => {
+                    if(result) {
+                        this.result = result.text;
+                    }
+                    if(err) {
+                        this.error = err;
+                    }
+                });
             } catch (err) {
                 console.log(err);
-                this.error = err.message;
             }
+        }
+    },
+    beforeUnmount() {
+        if (this.codeReader) {
+            this.codeReader.reset();
         }
     }
 }
