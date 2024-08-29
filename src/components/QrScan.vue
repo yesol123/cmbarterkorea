2c4eae1ed944b0bc8693697b958eb54fe69e4cb4
JiYu <146607445+cherrybbang@users.noreply.github.com>
2024-08-29 11:49:15 +0900

    update

--- src/components/QrScan.vue
+++ src/components/QrScan.vue
@@ -33,84 +33,15 @@ export default {
 }
 </script> -->
 
-<!-- <template>
-    <p>결제하기 - QR 스캔</p>
-    <video ref="video" width="90%" height="200" style="border: 1px solid red" autoplay></video>
-    <p v-if="result">Result: {{ result }}</p>
-    <p v-if="error">Error: {{ error }}</p>
-</template> -->
-<!-- 
-<script>
-// import { BrowserQRCodeReader } from '@zxing/browser';
-import { BrowserMultiFormatReader } from '@zxing/browser';
-
-export default {
-    data() {
-        return {
-            // imageUrl: '',
-            // result: '',
-            // error: ''
-            result : null,
-            codeReader : null,
-            error : ''
-        }
-    },
-    async mounted() {
-        // this.decodeQRCode();
-        this.codeReader = new BrowserMultiFormatReader();
-        this.StartScanner();
-    },
-    methods : {
-        // async decodeQRCode() {
-            
-        //     this.result = '';
-        //     this.error = '';
-
-        //     try {
-        //         const codeReader = new BrowserQRCodeReader();
-        //         const result = await codeReader.decodeFromImageUrl(this.imageUrl);
-        //         this.result = result.text;
-        //     } catch (err) {
-        //         console.log(err);
-        //         this.error = err.message;
-        //     }
-        // }
-        async StartScanner() {
-            try {
-                const videoElement = this.$refs.video;
-                const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
-                videoElement.srcObject = stream;
-
-                this.codeReader.decodeFromVideoDevice(null, videoElement, (result, err) => {
-                    if(result) {
-                        this.result = result.text;
-                    }
-                    if(err) {
-                        this.error = err;
-                    }
-                });
-            } catch (err) {
-                console.log(err);
-            }
-        }
-    },
-    beforeUnmount() {
-        if (this.codeReader) {
-            this.codeReader.reset();
-        }
-    }
-}
-</script> -->
-
 <template>
     <p>QR Scanner</p>
     <div style="width: 90%;">
         <StreamQrcodeBarcodeReader
             capture="shoot"
+            @result="onResult"
         />
         <!-- @loaded="onLoaded" -->
             <!-- @onloading="onLoading" -->
-            <!-- @result="onResult" -->
     </div>
     <p>Detected Code: {{ detectedCode }}</p>
 </template>
@@ -149,19 +80,11 @@ export default {
         const btn = document.querySelector('.btn-stream');
         btn.style.display = 'none';
         btn.click();
-        this.onResult();
     },
     methods : {
         onResult(result) {
             this.detectedCode = result;
         },
-        // Click() {
-        //     const btn = document.querySelector('.btn-stream');
-        //     btn.addEventListener('click', () => {
-        //         console.log('버튼클릭되었음');
-        //     });
-        //     this.onResult();
-        // }
     }
 }
 </script>
