cf0115595c6083ac64b2a837a6ac287a58e6aa92
ys <yesolg90@gmail.com>
2024-10-11 14:59:32 +0900

    운영정보 ing

---
+++ src/views/RoulettePage2.vue
@@ -0,0 +1,23 @@
+<template>
+
+<div class="roulette">
+  <div class="spinner"></div>
+  <div class="shadow"></div>
+  <div class="markers">
+    <div class="triangle">
+      
+    </div>
+  </div>
+  <div class="button">
+    <span>SPIN</span>
+  </div>
+</div>
+</template>
+    
+<script>
+
+</script>
+
+<style scope>
+
+</style>
