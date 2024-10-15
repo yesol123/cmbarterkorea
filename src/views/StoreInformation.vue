3730f860e71e2dbeb898f5d3b9b03c338ab1de00
ys <yesolg90@gmail.com>
2024-10-15 13:24:42 +0900

    매장관리 저장 작업중

--- src/views/StoreInformation.vue
+++ src/views/StoreInformation.vue
@@ -24,14 +24,13 @@
     <article>
       <div class="business_hours">
         <div class="flex_between">
-
           <p class="title holiday_txt m_B20">영업 시간</p>
         </div>
+
         <div class="flex_between">
           <div class="hours_box">
-
             <a-config-provider :locale="locale">
-              <a-time-picker class="timepicker hours_time" v-model:value="openTime" :format="format"
+              <a-time-picker class="timepicker hours_time" v-model:value="workHours1.openTime" :format="format"
                 dropdown-class-name="custom-time-picker-dropdown" />
             </a-config-provider>
             <!-- 
@@ -40,7 +39,7 @@
           <div class="hours_box">
 
             <a-config-provider :locale="locale">
-              <a-time-picker class="timepicker hours_time" v-model:value="closeTime" :format="format"
+              <a-time-picker class="timepicker hours_time" v-model:value="workHours1.closeTime" :format="format"
                 dropdown-class-name="custom-time-picker-dropdown" />
             </a-config-provider>
           </div>
@@ -48,91 +47,35 @@
 
         <div class="flex_between">
           <p class="title holiday_txt m_B20">휴게 시간</p>
-          <!-- <p class="hour_switch m_B20">
-              <input type="checkbox" id="rest_time1" class="hour_switch_check">
-              <label class="switch_label"> <span class="onf_btn"></span></label>
-            </p> -->
         </div>
         <div class="flex_between time_rest d_none">
           <div class="hours_box">
             <a-config-provider :locale="locale">
-              <a-time-picker class="timepicker hours_time" v-model:value="restStartTime" :format="format"
+              <a-time-picker class="timepicker hours_time" v-model:value="workHours1.restStartTime" :format="format"
                 dropdown-class-name="custom-time-picker-dropdown" />
             </a-config-provider>
           </div>
           <div class="hours_box">
 
             <a-config-provider :locale="locale">
-              <a-time-picker class="timepicker hours_time" v-model:value="restEndTime" :format="format"
+              <a-time-picker class="timepicker hours_time" v-model:value="workHours1.restEndTime" :format="format"
                 dropdown-class-name="custom-time-picker-dropdown" />
             </a-config-provider>
           </div>
         </div>
         <b class="info_note" id="timeError1">* 휴게시간은 운영시간 안으로만 설정가능합니다.</b>
-
         <ul class="join_state" id="day1">
-          <!-- input radio -->
-          <li>
+          <li v-for="day in days" :key="day.value">
             <label class="join_obj">
-              <input type="radio" name="sunday" onclick="deselect()">
-              <span>
-                일
-              </span>
-            </label>
-          </li>
-          <li>
-            <label class="join_obj">
-              <input type="radio" name="monday" onclick="deselect()">
-              <span>
-                월
-              </span>
-            </label>
-          </li>
-          <li>
-            <label class="join_obj">
-              <input type="radio" name="tuesday" onclick="deselect()">
-              <span>
-                화
-              </span>
-            </label>
-          </li>
-          <li>
-            <label class="join_obj">
-              <input type="radio" name="wednesday" onclick="deselect()">
-              <span>
-                수
-              </span>
-            </label>
-          </li>
-          <li>
-            <label class="join_obj">
-              <input type="radio" name="thursday" onclick="deselect()">
-              <span>
-                목
-              </span>
-            </label>
-          </li>
-          <li>
-            <label class="join_obj">
-              <input type="radio" name="friday" onclick="deselect()">
-              <span>
-                금
-              </span>
-            </label>
-          </li>
-          <li>
-            <label class="join_obj">
-              <input type="radio" name="saturday" onclick="deselect()">
-              <span>
-                토
-              </span>
+              <input type="checkbox" v-model="selectedDays1" :value="day.value">
+              <span>{{ day.label }}</span>
             </label>
           </li>
         </ul>
       </div>
     </article>
-    
-    <article  style="margin-top: 20px;">
+
+    <article style="margin-top: 20px;">
       <div class="business_hours">
         <div class="flex_between">
 
@@ -142,16 +85,14 @@
           <div class="hours_box">
 
             <a-config-provider :locale="locale">
-              <a-time-picker class="timepicker hours_time" v-model:value="openTime" :format="format"
+              <a-time-picker class="timepicker hours_time" v-model:value="workHours2.openTime" :format="format"
                 dropdown-class-name="custom-time-picker-dropdown" />
             </a-config-provider>
-            <!-- 
-            <img src="@/assets/icon_shevron_down.svg" alt="화살표" @click="toggleDropdown"> -->
           </div>
           <div class="hours_box">
 
             <a-config-provider :locale="locale">
-              <a-time-picker class="timepicker hours_time" v-model:value="closeTime" :format="format"
+              <a-time-picker class="timepicker hours_time" v-model:value="workHours2.closeTime" :format="format"
                 dropdown-class-name="custom-time-picker-dropdown" />
             </a-config-provider>
           </div>
@@ -159,84 +100,30 @@
 
         <div class="flex_between">
           <p class="title holiday_txt m_B20">휴게 시간</p>
-          <!-- <p class="hour_switch m_B20">
-              <input type="checkbox" id="rest_time1" class="hour_switch_check">
-              <label class="switch_label"> <span class="onf_btn"></span></label>
-            </p> -->
         </div>
         <div class="flex_between time_rest d_none">
           <div class="hours_box">
             <a-config-provider :locale="locale">
-              <a-time-picker class="timepicker hours_time" v-model:value="restStartTime" :format="format"
+              <a-time-picker class="timepicker hours_time" v-model:value="workHours2.restStartTime" :format="format"
                 dropdown-class-name="custom-time-picker-dropdown" />
             </a-config-provider>
           </div>
           <div class="hours_box">
 
             <a-config-provider :locale="locale">
-              <a-time-picker class="timepicker hours_time" v-model:value="restEndTime" :format="format"
+              <a-time-picker class="timepicker hours_time" v-model:value="workHours2.restEndTime" :format="format"
                 dropdown-class-name="custom-time-picker-dropdown" />
             </a-config-provider>
           </div>
         </div>
         <b class="info_note" id="timeError1">* 휴게시간은 운영시간 안으로만 설정가능합니다.</b>
 
-        <ul class="join_state" id="day1">
-          <!-- input radio -->
-          <li>
+
+        <ul class="join_state" id="day2">
+          <li v-for="day in days" :key="day.value">
             <label class="join_obj">
-              <input type="radio" name="sunday" >
-              <span>
-                일
-              </span>
-            </label>
-          </li>
-          <li>
-            <label class="join_obj">
-              <input type="radio" name="monday" onclick="deselect()">
-              <span>
-                월
-              </span>
-            </label>
-          </li>
-          <li>
-            <label class="join_obj">
-              <input type="radio" name="tuesday" onclick="deselect()">
-              <span>
-                화
-              </span>
-            </label>
-          </li>
-          <li>
-            <label class="join_obj">
-              <input type="radio" name="wednesday" onclick="deselect()">
-              <span>
-                수
-              </span>
-            </label>
-          </li>
-          <li>
-            <label class="join_obj">
-              <input type="radio" name="thursday" onclick="deselect()">
-              <span>
-                목
-              </span>
-            </label>
-          </li>
-          <li>
-            <label class="join_obj">
-              <input type="radio" name="friday" onclick="deselect()">
-              <span>
-                금
-              </span>
-            </label>
-          </li>
-          <li>
-            <label class="join_obj">
-              <input type="radio" name="saturday" onclick="deselect()">
-              <span>
-                토
-              </span>
+              <input type="checkbox" v-model="selectedDays2" :value="day.value">
+              <span>{{ day.label }}</span>
             </label>
           </li>
         </ul>
@@ -245,23 +132,23 @@
 
     <div class="flex_between">
       <p class="title">공휴일 / 국경일 휴무</p>
-      <label class="join_obj">
-        <input type="checkbox" name="saturday" onclick="deselect()">
-      </label>
+        <div class="checkbox-wrapper-2">
+        <input  class="sc-gJwTLC ikxBAC" type="checkbox" v-model="businessInfo.holiday" name="saturday">
+        </div>
     </div>
 
     <div>
       <p class="title">정기휴무</p>
       <div class="regular_holiday">
         <label class="for_select" for="selectWeek">
-          <select name="" id="selectWeek" class="select">
+          <select v-model="businessInfo.regularHoliday.frequency" name="" id="selectWeek" class="select">
             <option value="주기" selected>주기</option>
             <option value="매주">매주</option>
             <option value="격주">격주</option>
           </select>
         </label>
         <label class="for_select" for="selectDay">
-          <select name="" id="selectDay" class="select">
+          <select v-model="businessInfo.regularHoliday.day" name="" id="selectDay" class="select">
             <option value="요일" selected>요일</option>
             <option value="일">일요일</option>
             <option value="월">월요일</option>
@@ -279,11 +166,13 @@
       <p class="title">임시휴무</p>
       <div class="temporary_Closure">
         <a-config-provider :locale="locale">
-        <a-space direction="vertical" size="12">
-          <a-range-picker :presets="rangePresets" @change="onRangeChange" />
-        </a-space>
-      </a-config-provider>
-        <input class="coments" type="text" name="" id="" v-model="coments" placeholder="코멘트를 입력하세요">
+          <a-space direction="vertical" size="12">
+            <a-range-picker v-model="businessInfo.temporaryClosure.dateRange" :presets="rangePresets"
+              @change="onRangeChange" />
+          </a-space>
+        </a-config-provider>
+        <input class="coments" type="text" name="" id="" v-model="businessInfo.temporaryClosure.comment"
+          placeholder="코멘트를 입력하세요">
       </div>
     </div>
 
@@ -302,6 +191,26 @@ import { useResponseStore } from '@/store/response.js'
 export default {
   name: 'storeInformation',
   watch: {
+    selectedDays1(newSelectedDays) {
+    // selectedDays2와 중복되는 요일이 있는지 확인
+    const duplicatedDay = newSelectedDays.find(day => this.selectedDays2.includes(day));
+    
+    if (duplicatedDay) {
+      message.error('첫 번째와 두 번째 세트의 요일이 같을 수 없습니다.');
+      // 중복된 요일을 선택 해제
+      this.selectedDays1 = newSelectedDays.filter(day => day !== duplicatedDay);
+    }
+  },
+  selectedDays2(newSelectedDays) {
+    // selectedDays1과 중복되는 요일이 있는지 확인
+    const duplicatedDay = newSelectedDays.find(day => this.selectedDays1.includes(day));
+
+    if (duplicatedDay) {
+      message.error('첫 번째와 두 번째 세트의 요일이 같을 수 없습니다.');
+      // 중복된 요일을 선택 해제
+      this.selectedDays2 = newSelectedDays.filter(day => day !== duplicatedDay);
+    }
+  },
     restStartTime() {
       this.validateRestTime();
     },
@@ -311,24 +220,48 @@ export default {
   },
   data() {
     return {
-      openTime: dayjs('00:00', 'HH:mm'), // 영업 시작 시간
-      closeTime: dayjs('00:00', 'HH:mm'), // 영업 종료 시간
-      restStartTime: dayjs('00:00', 'HH:mm'), // 휴게 시작 시간
-      restEndTime: dayjs('00:00', 'HH:mm'), // 휴게 종료 시간
+      days: [
+        { value: '일', label: '일' },
+        { value: '월', label: '월' },
+        { value: '화', label: '화' },
+        { value: '수', label: '수' },
+        { value: '목', label: '목' },
+        { value: '금', label: '금' },
+        { value: '토', label: '토' }
+      ],
+      selectedDays1: [], // 체크된 요일을 저장할 배열
+      selectedDays2: [], // 체크된 요일을 저장할 배열
+      workHours1: {
+        day: '', // 첫 번째 시간 세트의 요일
+        openTime: dayjs('00:00', 'HH:mm'), // 첫 번째 시간 세트 영업 시작 시간
+        closeTime: dayjs('00:00', 'HH:mm'), // 첫 번째 시간 세트 영업 종료 시간
+        restStartTime: dayjs('00:00', 'HH:mm'), // 첫 번째 시간 세트 휴게 시작 시간
+        restEndTime: dayjs('00:00', 'HH:mm'), // 첫 번째 시간 세트 휴게 종료 시간
+      },
+      workHours2: {
+        day: '', // 첫 번째 시간 세트의 요일
+        openTime: dayjs('00:00', 'HH:mm'), // 첫 번째 시간 세트 영업 시작 시간
+        closeTime: dayjs('00:00', 'HH:mm'), // 첫 번째 시간 세트 영업 종료 시간
+        restStartTime: dayjs('00:00', 'HH:mm'), // 첫 번째 시간 세트 휴게 시작 시간
+        restEndTime: dayjs('00:00', 'HH:mm'), // 첫 번째 시간 세트 휴게 종료 시간
+      },
+      businessInfo: {
+        holiday: false, // 공휴일 유무
+        regularHoliday: {   //정기휴무
+          frequency: '주기',
+          day: '요일',
+        },
+        temporaryClosure: { //임시휴무
+          dateRange: [],
+          comment: '',
+        }
+      },
       format: 'HH:mm',
       locale: koKR,
       showTimeError: false, // 오류 메시지 표시 여부
-      selectedDays: {
-        sunday: false,
-        monday: false,
-        tuesday: false,
-        wednesday: false,
-        thursday: false,
-        friday: false,
-        saturday: false,
-      },
     };
   },
+
   methods: {
     validateRestTime() {
       // openTime, closeTime, restStartTime, restEndTime이 제대로 설정되었는지 먼저 확인
@@ -368,52 +301,56 @@ export default {
       }
       return disabledHours;
     },
-    saveData(){
+    saveData() {
       let store = useResponseStore();
-      
+
       const formData = new FormData();
       
+  
+
       formData.append('type', 'store_update3');
       formData.append('user_id', store.user_id);
-      formData.append('work_start_time1',); //시작시간
-      formData.append('work_end_time1',); //끝나는 시간
-      formData.append('rest_start_time1',); // 휴게시간 시작
-      formData.append('rest_end_time1',); //휴게시간 끝
-      formData.append('day1',); //날짜 선택 
-      formData.append('work_start_time2',); //시작시간
-      formData.append('work_end_time2',); //끝나는 시간
-      formData.append('rest_start_time2',); // 휴게시간 시작
-      formData.append('rest_end_time2',); //휴게시간 끝
-      formData.append('day2',); //날짜 선택
-      formData.append('holi',); //공휴일 유무
-      formData.append('reg_type',); //정기휴무 주기
-      formData.append('reg_day',); //정기휴무 요일
-      formData.append('datetimes',)// 임시휴무 날짜지정
-      formData.append('holiday_txt',)// 임시휴무 코멘트
+      formData.append('work_start_time1', this.workHours1.openTime.format(this.format)); //시작시간
+      formData.append('work_end_time1', this.workHours1.closeTime.format(this.format)); //끝나는 시간
+      formData.append('rest_start_time1', this.workHours1.restStartTime.format(this.format)); // 휴게시간 시작
+      formData.append('rest_end_time1', this.workHours1.restEndTime.format(this.format)); //휴게시간 끝
+      formData.append('day1', this.selectedDays1.join(',')); //날짜 선택 
 
+      formData.append('work_start_time2', this.workHours2.openTime.format(this.format)); //시작시간
+      formData.append('work_end_time2', this.workHours2.closeTime.format(this.format)); //끝나는 시간
+      formData.append('rest_start_time2', this.workHours2.restStartTime.format(this.format)); // 휴게시간 시작
+      formData.append('rest_end_time2', this.workHours2.restEndTime.format(this.format)); //휴게시간 끝
+      formData.append('day2', this.selectedDays2.join(',')); //날짜 선택 
+
+      formData.append('holi', this.businessInfo.holiday ? 'Y' : 'N'); //공휴일 유무
+      formData.append('reg_type', this.businessInfo.regularHoliday.frequency); //정기휴무 주기
+      formData.append('reg_day', this.businessInfo.regularHoliday.day); //정기휴무 요일
+      formData.append('datetimes', JSON.stringify(this.businessInfo.temporaryClosure.dateRange.join(',')))// 임시휴무 날짜지정
+      formData.append('holiday_txt', this.businessInfo.temporaryClosure.comment)// 임시휴무 코멘트
+
+      for (let [key, value] of formData.entries()) {
+  console.log(`${key}: ${value}`);
+} 
       const url = process.env.VUE_APP_API_URL;
 
       fetch(url + 'api/store/store_update.php', {
-            method : 'POST',
-            body : formData
-            })
-            .then(response => response.json())
-            .then(data => {
-               console.log('data',data);
-               
-            })
-
-
-
-
+        method: 'POST',
+        body: formData
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log('data', data);
+        })
 
     }
 
   }
 }
 </script>
-<style scoped>
 
+
+
+<style scoped>
 ul,
 a {
   list-style: none;
@@ -509,11 +446,12 @@ a {
   border-bottom: 3px solid #1749c2;
 }
 
-.title{
+.title {
   font-weight: bold;
 }
 
-.coments,.select{
+.coments,
+.select {
   padding: 8px;
   border: 1px solid #b1b1b1;
   border-radius: 5px;
@@ -618,7 +556,7 @@ select {
   /* 텍스트 선택 불가능 */
 }
 
-.join_obj input[type="radio"] {
+.join_obj input[type="checkbox"] {
   display: none;
   /* 라디오 버튼 숨기기 */
 }
@@ -629,7 +567,7 @@ select {
 }
 
 /* 선택된 상태에서 스타일 */
-.join_obj input[type="radio"]:checked+span {
+.join_obj input[type="checkbox"]:checked+span {
   background-color: #1749c2;
   /* 선택된 상태의 배경색 */
   color: white;
@@ -654,7 +592,7 @@ select {
   width: 100%;
 }
 
-.save_btn{
+.save_btn {
   width: 100%;
   margin: 15px 0;
   padding: 8px 0;
@@ -664,13 +602,80 @@ select {
   color: white;
 }
 
+/* 국경일 checkbox css */
+.checkbox-wrapper-2 .ikxBAC {
+    appearance: none;
+    background-color: #dfe1e4;
+    border-radius: 72px;
+    border-style: none;
+    flex-shrink: 0;
+    height: 20px;
+    margin: 0;
+    position: relative;
+    width: 30px;
+  }
+
+  .checkbox-wrapper-2 .ikxBAC::before {
+    bottom: -6px;
+    content: "";
+    left: -6px;
+    position: absolute;
+    right: -6px;
+    top: -6px;
+  }
+
+  .checkbox-wrapper-2 .ikxBAC,
+  .checkbox-wrapper-2 .ikxBAC::after {
+    transition: all 100ms ease-out;
+  }
+
+  .checkbox-wrapper-2 .ikxBAC::after {
+    background-color: #fff;
+    border-radius: 50%;
+    content: "";
+    height: 14px;
+    left: 3px;
+    position: absolute;
+    top: 3px;
+    width: 14px;
+  }
+
+  .checkbox-wrapper-2 input[type=checkbox] {
+    cursor: default;
+  }
+
+  .checkbox-wrapper-2 .ikxBAC:hover {
+    background-color: #c9cbcd;
+    transition-duration: 0s;
+  }
+
+  .checkbox-wrapper-2 .ikxBAC:checked {
+    background-color: #007bff;
+    /* background-color: #6e79d6; */
+  }
+
+  .checkbox-wrapper-2 .ikxBAC:checked::after {
+    background-color: #fff;
+    left: 13px;
+  }
+
+  .checkbox-wrapper-2 :focus:not(.focus-visible) {
+    outline: 0;
+  }
+
+  .checkbox-wrapper-2 .ikxBAC:checked:hover {
+    background-color: #007bff;
+
+    /* background-color: #535db3; */
+  }
+
+
 
 @media (max-width: 360px) {
   .join_state {
-  gap: 5px;
+    gap: 5px;
   }
 
 }
-
 
 </style>
