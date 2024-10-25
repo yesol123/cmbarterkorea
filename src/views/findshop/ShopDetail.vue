7abd733e0c20c74a12e224fdc8192fc7699a1684
ys <yesolg90@gmail.com>
2024-10-25 14:25:08 +0900

    가맹점 상세보기 매장이미지 ing..

--- src/views/findshop/ShopDetail.vue
+++ src/views/findshop/ShopDetail.vue
@@ -1,11 +1,11 @@
 <template>
-     <header>
+    <header>
         <button type="button" class="goback_btn" @click="toFindShop()">
             <img src="@/assets/go_back_btn.png"></button>
         <h3>가맹점 상세정보</h3>
     </header>
 
-    <main>
+    <section>
         <div class="img">
             <div :style="{ backgroundImage: `url(${currentBanner})` }"></div>
             <p>{{ shopinfo.store_business_state_value }}</p>
@@ -17,11 +17,13 @@
             </div>
             <div class="call_map">
                 <p>{{ shopinfo.user_cm_use }} CM 가능</p>
-                <button type="button"><a :href="`tel:${shopinfo.store_phone}`"><img src="@/assets/call.png" alt="전화이미지"></a></button>
-                <button type="button" @click="GetStoreIndex(shopinfo.store_index)"><img src="@/assets/location.png" alt="위치이미지"></button>
+                <button type="button"><a :href="`tel:${shopinfo.store_phone}`"><img src="@/assets/call.png"
+                            alt="전화이미지"></a></button>
+                <button type="button" @click="GetStoreIndex(shopinfo.store_index)"><img src="@/assets/location.png"
+                        alt="위치이미지"></button>
             </div>
         </div>
-        <div class="space"> 
+        <div class="space">
             정보
         </div>
         <div class="etc">
@@ -41,8 +43,18 @@
 
             <p>소개</p>
             <p>{{ shopinfo.store_memo }}</p>
+
+            <p>매장 이미지</p>
+
+            <ul class="imgbox_ul">
+                <li v-for="(image, index) in imglist2" :key="index">
+                    <div class="imgbox">
+                        <img :src="image.store_image" :alt="'Image ' + index">
+                    </div>
+                </li>
+            </ul>
         </div>
-    </main>
+    </section>
 </template>
 
 <script>
@@ -51,19 +63,21 @@ import { useResponseStore } from '@/store/response.js'
 export default {
     data() {
         return {
-            shopinfo : '',
-            imglist : [],
-            bannerIndex : 0
+            shopinfo: '',
+            imglist: [],
+            bannerIndex: 0,
+            imglist2:[]
         }
     },
     mounted() {
         this.GetShopInfo();
         this.GetImage();
-        this.bannerSliding()
+        this.bannerSliding();
+        this.getShowImg()
     },
-    methods : {
+    methods: {
         toFindShop() {
-            this.$router.push({ path : '/findshop' });
+            this.$router.push({ path: '/findshop' });
         },
         // 가맹점정보 불러오기
         GetShopInfo() {
@@ -78,11 +92,11 @@ export default {
                 method: 'POST',
                 body: formData
             })
-            .then(response => response.json())
-            .then(data => {
-                // console.log(data.msg[0].store_name);
-                this.shopinfo = data.msg[0];
-            })
+                .then(response => response.json())
+                .then(data => {
+                    // console.log(data.msg[0].store_name);
+                    this.shopinfo = data.msg[0];
+                })
         },
         // 가맹점이미지 불러오기
         GetImage() {
@@ -97,14 +111,14 @@ export default {
                 method: 'POST',
                 body: formData
             })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data.msg);
-                for(let i=0; i<data.msg.length; i++) {
-                    this.imglist.push(data.msg[i].store_image);
-                    console.log(this.imglist);
-                }
-            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data.msg);
+                    for (let i = 0; i < data.msg.length; i++) {
+                        this.imglist.push(data.msg[i].store_image);
+                        console.log(this.imglist);
+                    }
+                })
         },
         // 이미지 슬라이드
         bannerSliding() {
@@ -121,8 +135,27 @@ export default {
 
             this.$router.push({ path: '/mapdetail' });
         },
+        //매장 이미지 보여주기
+        getShowImg(){
+        let store = useResponseStore();
+        const formData= new FormData();
+        formData.append('type', 'store_image_select');
+        formData.append('user_index', store.user_index);
+        
+        const url = process.env.VUE_APP_API_URL;
+        fetch(url + 'api/store/store_update.php', {
+            method: 'POST',
+            body: formData
+        })
+            .then(response => response.json())
+            .then(result => {
+                console.log('222 ', result);
+                console.log('222 ', result.msg);
+                this.imglist2 = result.msg
+            })
+        }
     },
-    computed : {
+    computed: {
         currentBanner() {
             return this.imglist[this.bannerIndex];
         },
@@ -137,64 +170,84 @@ export default {
     background-color: #fff;
     border: 1px solid #fff;
 }
+
 .goback_btn img {
-    width: 100%; height: 100%;
+    width: 100%;
+    height: 100%;
 }
-main {
+
+section {
     width: 100%;
     margin-top: 60px;
     padding: 0 15px;
     margin-bottom: 100px;
     /* border: 1px solid red; */
 }
+
 .img {
     position: relative;
-    width: 100%; height: 250px;
+    width: 100%;
+    height: 250px;
     /* border: 1px solid blue; */
 }
-.img > div {
+
+.img>div {
     display: inline-block;
-    width: 100%; height: 100%;
+    width: 100%;
+    height: 100%;
     /* background-image: url(/src/assets/1.jpg); */
     background-repeat: no-repeat;
     background-size: contain;
 }
-.img > p {
+
+.img>p {
     position: absolute;
-    bottom: 0; left: 0;
-    width: 100%; height: 35px;
-    text-align: center; line-height: 35px;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 35px;
+    text-align: center;
+    line-height: 35px;
     font-weight: bold;
     border-radius: 15px;
     background-color: rgb(225, 244, 196);
     border: none;
 }
+
 .info {
     display: flex;
     justify-content: space-between;
-    width: 100%; height: 130px;
+    width: 100%;
+    height: 130px;
     padding: 0 15px;
     /* border: 1px solid blue; */
 }
+
 .name {
     width: 50%;
 }
-.name > p {
+
+.name>p {
     font-weight: bold;
     font-size: 1.2rem;
 }
-.name > p:nth-of-type(2) {
+
+.name>p:nth-of-type(2) {
     font-weight: normal;
     font-size: 1rem;
     color: #ccc;
 }
+
 .call_map {
     width: 50%;
     text-align: right;
 }
-.call_map > p {
-    width: 80%; height: 25%;
-    text-align: center; line-height: 33px;
+
+.call_map>p {
+    width: 80%;
+    height: 25%;
+    text-align: center;
+    line-height: 33px;
     color: #fff;
     font-size: 0.8rem;
     margin-left: 30%;
@@ -202,7 +255,8 @@ main {
     border: none;
     background-color: #1bce0b;
 }
-.call_map > button {
+
+.call_map>button {
     width: 20%;
     padding: 5px;
     border: 1px solid #1bce0b;
@@ -214,18 +268,47 @@ main {
     /* 최대 높이 제한 */
     margin-left: 10px;
 }
+
 .call_map button img {
     width: 80%;
 }
+
 .space {
-    width: 100%; height: 50px;
-    font-weight: bold;text-align: center; line-height: 50px;
+    width: 100%;
+    height: 50px;
+    font-weight: bold;
+    text-align: center;
+    line-height: 50px;
     border-bottom: 2px solid #1bce0b;
 }
+
 .etc {
     width: 100%;
 }
-.etc > p {
-    width: 100%; height: 20px;
+
+.etc>p {
+    width: 100%;
+}
+
+.imgbox_ul{
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+}
+
+.imgbox_ul li{
+    border: 1px solid #ccc;
+    border-radius: 5px;
+}
+
+.imgbox{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+
+.imgbox img{
+    width: 100%;
 }
 </style>
