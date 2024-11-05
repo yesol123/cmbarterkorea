be3b407ba44764ca917f11a6da2151cf8e9aff35
ys <yesolg90@gmail.com>
2024-11-05 18:25:58 +0900

    ddd

--- src/views/LogInPage.vue
+++ src/views/LogInPage.vue
@@ -87,7 +87,7 @@ export default {
                 return false;
             }
             if(this.id && this.password) {
-                
+               ///awefawefawef 
                 let store = useResponseStore();
                 // const refs = this.$refs.child;
 
